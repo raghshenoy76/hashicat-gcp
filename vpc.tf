923d34b88c803c5a33d4480552c1c3cf91f44187
raghshenoy76 <raghshenoy@icloud.com>
2021-11-04 12:58:59 +0000

    added vpc modules

---
+++ vpc.tf
@@ -0,0 +1,18 @@
+module "network" {
+  source  = "app.terraform.io/db-shenoy/network/google"
+  version = "3.4.0"
+  # insert required variables here
+
+    project_id   = var.project
+    network_name = "ex-vpc"
+    routing_mode = "GLOBAL"
+
+    subnets = [
+        {
+        subnet_name   = "gaurav-subnet"
+        subnet_ip     = "10.100.10.0/24"
+        subnet_region = var.region
+        }
+    ]
+
+}
